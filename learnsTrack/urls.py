072b5a89829c617507288964b43f51223d04da1f
unknown <chers94@gmail.com>
2025-01-14 15:20:15 +0200

    Was added 'topic' template and all related changes

--- learnsTrack/urls.py
+++ learnsTrack/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name ='index'),
     #Show all topics page
     path('topics/', views.topics, name = 'topics'),
+    #Page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
 ]
--- learnsTrack/views.py
+++ learnsTrack/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Topic
 
+#Home page
 def index(request):
     return render(request, 'learns_track/index.html')
 
+#Page that show all topics
 def topics(request):
-    #Page that show all topics
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learns_track/topics.html', context)
+
+#A single topic and all its entries
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries':entries}
+    return render(request, 'learns_track/topic.html', context)
